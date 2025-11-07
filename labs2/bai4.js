fd42d6d295243059c21d5e2837d87c3f04ef8bd1
Tran Tuan Hung <hungtran092006@gmail.com>
2025-11-07 14:40:03 +0700

    update bai4.js

--- labs2/bai4.js
+++ labs2/bai4.js
@@ -1,4 +1,4 @@
-// Tạo HTML template cho card sản phẩm
+
 const product = {
   name: "iPhone 15",
   price: 20000000,
@@ -6,10 +6,10 @@ const product = {
   inStock: true,
 };
 
-// Tính giá sau giảm
+
 const finalPrice = product.price * (1 - product.discount / 100);
 
-// Tạo template HTML
+
 const productCard = `
 <div class="product-card">      
     <h2>${product.name}</h2> 
@@ -17,6 +17,6 @@ const productCard = `
     <p>Giá: ${finalPrice} VND</p>
     <p>${product.inStock ? "Còn hàng" : "Hết hàng"}</p>
 </div>
-`; // Viết template ở đây
+`; 
 
 console.log(productCard); 
