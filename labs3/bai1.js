8183fd4e410d711debbc058382f6dd19f1b8781b
Tran Tuan Hung <hungtran092006@gmail.com>
2025-11-07 23:10:47 +0700

    update bai 3 bai 4

--- labs3/bai1.js
+++ labs3/bai1.js
@@ -21,3 +21,32 @@ document.addEventListener("click", () => {
 console.log(createUser());
 
 // bai3
+
+const mergeArrays = (...arrays) => [...arrays.flat()];
+const sumAll = (...numbers) => numbers.reduce((sum, num) => sum + num, 0);
+const createProduct = ({
+  name = "Unknown",
+  price = 0,
+  brand = "No brand",
+  inStock = true
+} = {}) => ({
+  name,
+  price,
+  brand,
+  inStock
+});
+
+// bai 4
+// Tạo hàm shoppingCart với:
+
+// Tham số đầu tiên là tên khách hàng
+// Các tham số tiếp theo là các sản phẩm
+// Sử dụng rest parameters để thu thập sản phẩm
+// Trả về object chứa thông tin đơn hàng
+const shoppingCart = (Name, ...products) => ({
+  Name,
+  products
+});
+
+const order = shoppingCart("Alice", "táo", "chuối", "cam");
+console.log(order); 
