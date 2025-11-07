2d487bb94b3cee2267573ba6e28d02db293d8103
Tran Tuan Hung <hungtran092006@gmail.com>
2025-11-07 14:41:10 +0700

    update bai4.js

--- labs2/bai4.js
+++ labs2/bai4.js
@@ -1,15 +1,10 @@
-
 const product = {
   name: "iPhone 15",
   price: 20000000,
   discount: 10,
   inStock: true,
 };
-
-
 const finalPrice = product.price * (1 - product.discount / 100);
-
-
 const productCard = `
 <div class="product-card">      
     <h2>${product.name}</h2> 
@@ -18,5 +13,4 @@ const productCard = `
     <p>${product.inStock ? "Còn hàng" : "Hết hàng"}</p>
 </div>
 `; 
-
 console.log(productCard); 
